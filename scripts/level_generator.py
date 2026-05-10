576cdcd844ccdaa06eece4b010a8584bf79a1720
Abdurrahman Alyajouri <abdu.alyajouri@gmail.com>
2026-05-10 04:24:02 -0700

    Some more setup

---
+++ scripts/level_generator.py
@@ -0,0 +1,4 @@
+#Filename: level_generator.py
+#Programmer: Abdurrahman Alyajouri
+#Date: 5/10/2026
+#Purpose: To provide a simple interface to procedurally generate levels with rooms for robot simulation in gazebosim.
